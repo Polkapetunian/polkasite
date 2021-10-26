bd69ca824a09cda143e4755ee930d8e47b3605dc
Polkapetunian <70851837+Polkapetunian@users.noreply.github.com>
2021-10-26 15:15:59 +0200

    changed index.js according to react-snap instructions

--- frontend/polkasite/src/index.js
+++ frontend/polkasite/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { hydrate, render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 
 ReactDOM.render(
   <React.StrictMode>
